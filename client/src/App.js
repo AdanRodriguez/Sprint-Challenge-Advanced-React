ea3372930740f65555425871ef8cce73e2ebf38b
Adan <51211285+Adan011@users.noreply.github.com>
2019-09-06 13:08:12 -0500

    MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PlayerCard from "./component/PlayerCard";
+import Nav from "./component/Nav";
 
-function App() {
+class App extends React.Component{
+  constructor() {
+    super();
+    this.state= {
+    players: [],
+  }
+}
+
+  componentDidMount() {
+    fetch("http://localhost:5000/api/players")
+    .then(res => res.json())
+    .then(res => this.setState({players: res}))
+    .catch(err => console.log("error"));
+  }
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+        <h1>Women's World Cup</h1>
+        <Nav/>
+        <div>{this.state.players.map((player) => {
+                return <PlayerCard key={player.id} 
+                                  player ={player} />
+            })}
+        </div>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
